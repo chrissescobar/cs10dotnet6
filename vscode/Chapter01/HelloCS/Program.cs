510a5fac0e045604ae00b03342457c3c9ed24b39
chrissescobar <escobach@kean.edu>
2022-02-11 11:40:48 -0500

    https://github.com/chrissescobar/SpecialTopics.git

--- vscode/Chapter01/HelloCS/Program.cs
+++ vscode/Chapter01/HelloCS/Program.cs
@@ -6,7 +6,9 @@ namespace HelloCS
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i <= 7; i++) {
             Console.WriteLine("Hello, C#!");
+            }
         }
     }
 }
